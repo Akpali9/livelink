b6c78cd5b38312e5b259af4c96fdaa814a337ecb
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-04 22:20:37 +0100

    Update routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -5,7 +5,7 @@ import { CreatorLogin } from "./screens/creator-login";
 import { BusinessLogin } from "./screens/business-login";
 import { Browse } from "./screens/browse";
 import { Profile } from "./screens/profile";
-import {AdminDashboard} from "./screen/admin-dashboard";
+// import {AdminDashboard} from "./screen/admin-dashboard";
 import { Dashboard } from "./screens/dashboard";
 import { BusinessDashboard } from "./screens/business-dashboard";
 import { BusinessProfile } from "./screens/business-profile";
@@ -45,7 +45,7 @@ const routes: RouteObject[] = [
     children: [
       { index: true, Component: Home },
       { path: "login/portal", Component: LoginPortal },
-      { path: "admin", Component: AdminDashboard },
+      // { path: "admin", Component: AdminDashboard },
       { path: "login/creator", Component: CreatorLogin },
       { path: "login/business", Component: BusinessLogin },
       { path: "browse", Component: Browse },
