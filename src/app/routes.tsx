ecacdcb5ff3f1d13849a69ea08212c29bffcbec3
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-04 15:51:22 +0100

    Add files via upload

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouteObject } from "react-router-dom";
+import { createBrowserRouter, RouteObject } from "react-router";
 import { Home } from "./screens/home";
 import { LoginPortal } from "./screens/login-portal";
 import { CreatorLogin } from "./screens/creator-login";
