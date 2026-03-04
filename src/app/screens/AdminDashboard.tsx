47aa529e3496ebfec0e5065649e7c99f62c77ecf
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-04 22:14:15 +0100

    Create AdminDashboard.tsx

---
+++ src/app/screens/AdminDashboard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AdminDashboard = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white px-6">
+      <h1 className="text-3xl font-black mb-4">Super Admin Dashboard</h1>
+      <p>Welcome, Super Admin! Manage users and campaigns here.</p>
+    </div>
+  );
+};
+
+export default AdminDashboard;
