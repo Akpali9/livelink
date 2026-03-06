c8b7077936ab654636b5a499c2856b2f3f23e276
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-03-06 08:54:44 +0100

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,5 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-
-  import React from "react";
-import { AuthProvider } from "../src/app/lib/contexts/AuthContext.tsx";
-import "../src/styles/index.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
